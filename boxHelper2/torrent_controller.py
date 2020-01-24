9165cbf90ce534e395e1fbdf1460b77b82038bf0
SpereShelde <spereshelde@gmail.com>
2020-01-23 23:24:30 -0500

    Web server left

--- boxHelper2/torrent_controller.py
+++ boxHelper2/torrent_controller.py
@@ -1,32 +1,30 @@
 import configparser
+import logging
 
-from boxHelper2.auto_torrent_collector import AutoTorrentCollector
+from boxHelper2.torrent_collector import TorrentCollector
 
-
-class TorrentController():
+class TorrentController:
 
     def start(self):
         config = configparser.RawConfigParser()
         config.read("../config.ini", encoding="utf-8")
         sites_amount = config.getint('sites', 'sites_amount')
-        headers = {
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        }
+        logging.basicConfig(level=logging.DEBUG,
+                            filename="../boxHelper.log",
+                            datefmt='%Y/%m/%d %H:%M:%S',
+                            format='%(asctime)s - %(levelname)s - %(module)s@%(lineno)d : %(message)s')
+        logger = logging.getLogger()
+
         collectors = []
         for i in range(1, sites_amount+1):
-            headers['Cookie'] = config.get('sites', 'cookie_' + str(i))
-            c = AutoTorrentCollector(i, config.get('sites', 'url_' + str(i)), headers,
-                                     config.get('sites', 'rss_' + str(i)), config.getint('sites', 'strength_' + str(i)),
-                                     config.getint('sites', 'cycle_' + str(i)))
+            c = TorrentCollector(i, config.get('sites', 'url_' + str(i)),
+                                 config.get('sites', 'rss_' + str(i)), config.getint('sites', 'strength_' + str(i)),
+                                 config.getint('sites', 'cycle_' + str(i)), logger)
             collectors.append(c)
-        print("01: %d collectors" % len(collectors))
         for collector in collectors:
-            collector.run()
+            collector.start()
 
 if __name__ == '__main__':
+    open("../boxHelper.log","w").close()
     t = TorrentController()
     t.start()
